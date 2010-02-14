44e8e86a7787021b4854b6a211cd0fa45da43a2b
jyang <jyang@node.mtv.corp.google.com>
2010-02-13 18:57:44 -0800

    Added alternative pattern for interceptors.  Handlers still synchronous.

--- test/test-handler-chain.js
+++ test/test-handler-chain.js
@@ -19,3 +19,58 @@ assert.equal('hello,world', handler('world'));
 
 handler = csf.handler.createChain([addB, addA, addB, hello]);
 assert.equal('hello,world[b][a][b]', handler('world'));
+
+// alternative interceptor pattern
+
+function openA(request) {
+  return request + '<a>';
+};
+
+function closeA(response) {
+  return response + '</a>'
+};
+
+function openB(request) {
+  return request + '<b>';
+};
+
+function closeB(response) {
+  return response + '</b>'
+};
+
+function echo(request) {
+  return request;
+}
+
+var interceptor = {
+  chain: csf.handler.createChain,
+  around: function(before, after) {
+    return function(request, intercepted) {
+      return after(intercepted(before(request)));
+    }
+  },
+  before: function(before) {
+    return function(request, intercepted) {
+      return intercepted(before(request));
+    };
+  },
+  after: function(after) {
+    return function(request, intercepted) {
+      return after(intercepted(request));
+    };
+  }
+};
+
+var handler = interceptor.chain([
+  interceptor.around(openA, closeA),
+  interceptor.around(openB, closeB),
+  echo
+]);
+assert.equal('world<a><b></b></a>', handler('world'));
+
+var handler = interceptor.chain([
+  interceptor.before(openA),
+  interceptor.after(closeB),
+  echo
+]);
+assert.equal('world<a></b>', handler('world'));
