3378e94f2c868ace1351630303e0d41aa53de55f
jyang <jyang@node.mtv.corp.google.com>
2010-02-13 17:51:36 -0800

    Checkpoint for version supporting synchronous handlers.  Handler patter:
    handler = function(request, opt_intercepted) {
      return opt_intercepted(request);
    };

--- sample/node-server.js
+++ sample/node-server.js
@@ -1,18 +1,17 @@
 var sys = require('sys');
-var http = require('http');
-var url = require('url');
 var csf = require('../src/cornerstone');
 
-function echoPath(request) {
-  return {output: {body: 'Hello, ' + request.input.path + '!'}, status: 200};
+function echoPath(request, handleResponse) {
+  handleResponse({
+		output:{
+			body: 'Hello, ' + request.input.path + '!'
+		},
+		status: 200
+	});
 };
 
-var rootHandler = csf.handler.createChain([
-  csf.handler.dumpRequestResponse,
-  csf.handler.handlePathPrefixArray.create([
-      ['/hello', echoPath], ['/world', echoPath]
-  ])
-]);
-
-csf.server.createNodeServer(rootHandler, 8000);
+csf.server.start([
+  ['/hello', echoPath],
+	['/world', echoPath]
+], 8000);
 sys.puts('Cornestone Server for Node @ http://localhost:8000/');
--- src/cornerstone.js
+++ src/cornerstone.js
@@ -12,19 +12,19 @@ var error = {
 
 /**
  * Resolves handler by looking up a path handler map.
- * this: pathHandlers
+ * this.pathPrefixHandlers
  */
 function handlePathPrefixArray(request) {
   if (request && request.input && request.input.path) {
-    for (var i = 0; i < this.pathHandlers.length; i++) {
-      var pathHandler = this.pathHandlers[i];
-      var pathPrefix = pathHandler[0];
-      var handler = pathHandler[1];
+    for (var i = 0; i < this.pathPrefixHandlers.length; i++) {
+      var pathPrefixHandler = this.pathPrefixHandlers[i];
+      var pathPrefix = pathPrefixHandler[0];
       if (request.input.path.indexOf(pathPrefix) == 0 &&
           (request.input.path.length == pathPrefix.length ||
               pathPrefix.charAt(pathPrefix.length - 1) == '/' ||
               request.input.path.charAt(pathPrefix.length) == '/')) {
         // path is proper prefix of request URL
+        var handler = pathPrefixHandler[1];
         return handler(request);
       }
     };
@@ -35,15 +35,20 @@ function handlePathPrefixArray(request) {
 
 /**
  * Creates a handlePathPrefixArray.
- * @param pathHandlers e.g. [['/abc', handler1], ['/', handler2]]
+ * @param pathPrefixHandlers Array of [pathPrefix, handler].
+ *     E.g. [['/abc', handler1], ['/', handler2]]
  */
-handlePathPrefixArray.create = function(pathHandlers) {
-  return handlePathPrefixArray.bind({pathHandlers: pathHandlers});
+handlePathPrefixArray.create = function(pathPrefixHandlers) {
+  return handlePathPrefixArray.bind({pathPrefixHandlers: pathPrefixHandlers});
 };
 
 // -----
 // Chain
 
+/**
+ * Creates a handler equivalent to calling all handlers in sequence.
+ * @param {Object} handlers Handlers out of which to create chain of handlers
+ */
 createChain = function(handlers) {
   if (handlers.length == 0) {
     throw new Error('empty handlers');
@@ -86,10 +91,29 @@ Function.prototype.bind = function(selfObj, var_args) {
   }
 };
 
+function toIsoDateString(d) {
+  function pad(n) {
+    return n < 10 ? '0' + n : n;
+  };
+  function pad3(n) {
+    return n < 10 ? '00' + n : n < 100 ? '0' + n : n;
+  };
+  return d.getUTCFullYear() + '-' + pad(d.getUTCMonth() + 1) + '-' +
+      pad(d.getUTCDate()) + 'T' + pad(d.getUTCHours()) + ':' +
+      pad(d.getUTCMinutes()) + ':' + pad(d.getUTCSeconds()) + '.' +
+      pad3(d.getUTCMilliseconds()) + 'Z';
+};
+
+function log(message) {
+  sys.puts(toIsoDateString(new Date()) + ' ' + message);
+};
+
 function dumpRequestResponse(request, nextHandler) {
-  sys.puts('request@' + new Date() + ': ' + sys.inspect(request));
+  log('request: ' + sys.inspect(request));
+
   var response = nextHandler(request);
-  sys.puts('response@' + new Date() + ': ' + sys.inspect(response));
+
+  log('response: ' + sys.inspect(response));
   return response;
 };
 
@@ -103,7 +127,9 @@ createNodeServer = function(rootHandler, port, opt_host) {
       header: request.headers,
       input: {
         url: requestUrl, path: requestUrl.pathname, body: request.body
-      }
+      },
+      __request: request,
+      __response: response
     };
 
     var responseMessage = rootHandler(requestMessage);
@@ -112,6 +138,20 @@ createNodeServer = function(rootHandler, port, opt_host) {
     response.sendBody(responseMessage.output.body);
     response.finish();
   }).listen(port, opt_host);
+};
+
+/**
+ * Starts one root context with some path handlers.
+ * @param {Object} pathPrefixHandlers e.g. [['/abc', handler1], ['/', handler2]]
+ * @param {Object} port
+ * @param {Object} opt_host
+ */
+start = function(pathPrefixHandlers, port, opt_host) {
+  var rootHandler = createChain([
+      dumpRequestResponse,
+      handlePathPrefixArray.create(pathPrefixHandlers)
+  ]);
+  createNodeServer(rootHandler, port, opt_host);
 };
 
 // -------
@@ -126,5 +166,10 @@ exports.handler = {
 };
 
 exports.server = {
-  createNodeServer: createNodeServer
+  createNodeServer: createNodeServer,
+  start: start
+};
+
+exports.util = {
+  log: log
 };
