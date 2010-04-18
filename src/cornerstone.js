dd687cf211977e5ab3064fce634587ef551d0e3b
jyang <jyang@node.mtv.corp.google.com>
2010-04-17 23:35:29 -0700

    Added proxy handler.

--- src/cornerstone.js
+++ src/cornerstone.js
@@ -1,10 +1,11 @@
 var sys = require('sys');
+var url = require('url');
 var http = require('http');
 
 var CSF_TRACE = process.ENV.CSF_TRACE;
 
-// ---------------------
-// handlePathPrefixArray
+// -----------------
+// Built-in handlers
 
 /**
  * Resolves handler by looking up a path handler map.
@@ -20,6 +21,7 @@ function handlePathPrefixArray(request, handleResponse) {
               pathPrefix.charAt(pathPrefix.length - 1) == '/' ||
               request.input.path.charAt(pathPrefix.length) == '/')) {
         // path is proper prefix of request URL
+        request.input.pathPrefix = pathPrefix;
         var handler = pathPrefixHandler[1];
         handler(request, handleResponse);
         return;
@@ -40,14 +42,16 @@ handlePathPrefixArray.create = function(pathPrefixHandlers) {
   return handlePathPrefixArray.bind({pathPrefixHandlers: pathPrefixHandlers});
 };
 
-// --------
-// handlers
+/**
+ * Dummy for non-existent handler "chain".
+ */
+chain = {};
 
 /**
  * Creates a handler equivalent to calling all handlers in sequence.
  * @param {Object} handlers Handlers out of which to create chain of handlers
  */
-createHandlerChain = function(handlers) {
+chain.create = function(handlers) {
   if (handlers.length == 0) {
     throw new Error('no handlers');
   } else if (handlers.length == 1) {
@@ -64,6 +68,44 @@ createHandlerChain = function(handlers) {
     }
     return intercepted[0];
   }
+};
+
+/* Proxies a request.
+ * this.destHost is destination host name.
+ * this.destPort is destination port number.
+ * this.destPathPrefix is destination URL prefix.
+ * For example, if proxy handler is configured for /a/b/c and destination prefix
+ * is http://host:port/x/y/z, then a request to /a/b/c/1 is proxy to
+ * http://host:port/x/y/z/1.
+ */
+proxy = function(request, handleResponse) {
+  var destPath = this.destPathPrefix +
+      request.input.path.substring(request.input.pathPrefix.length);
+  var client = http.createClient(this.destPort, this.destHost);
+  var proxyRequest = client.request('GET', destPath, {host: this.destHost});
+  proxyRequest.addListener('response', function (proxyResponse) {
+    request.__response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+    proxyResponse.setBodyEncoding('utf8');
+    proxyResponse.addListener('data', function (chunk) {
+      request.__response.write(chunk, 'utf8');
+    });
+    proxyResponse.addListener('end', function() {
+      handleResponse();
+    });
+  });
+  proxyRequest.end();
+};
+
+/**
+ * Creates a proxy handler.
+ * @param {String} destHost Destination host name
+ * @param {String} destPort Destination port number
+ * @param {String} destPathPrefix Destination path prefix
+ * @return Proxy handler created
+ */
+proxy.create = function(destHost, destPort, destPathPrefix) {
+  return proxy.bind({destHost: destHost, destPort: destPort,
+      destPathPrefix: destPathPrefix});
 };
 
 createAroundFilter = function(before, after) {
@@ -86,6 +128,19 @@ createAfterFilter = function(after) {
       handleResponse(after(response));
     });
   };
+};
+
+// ----------------
+// Built-in filters
+
+function dumpRequest(request){
+  log('request: ' + sys.inspect(request));
+  return request;
+};
+
+function dumpResponse(response) {
+  log('response: ' + sys.inspect(response));
+  return response;
 };
 
 // -----------
@@ -132,18 +187,6 @@ function output(status, message, opt_headers) {
   return {output: {body: message}, status: status, headers: opt_headers};
 }
 
-// filters
-
-function dumpRequest(request){
-  log('request: ' + sys.inspect(request));
-  return request;
-};
-
-function dumpResponse(response) {
-  log('response: ' + sys.inspect(response));
-  return response;
-};
-
 // ----
 // Node
 
@@ -160,9 +203,9 @@ startHandler = function(handler, port, opt_host) {
   http.createServer(function(request, response) {
     var start = Date.now();
 
-    var chain = createHandlerChain([
+    var handlerChain = chain.create([
       createBeforeFilter(function(request) {
-        var requestUrl = require('url').parse(request.url, true);
+        var requestUrl = url.parse(request.url, true);
         return {
           header: request.headers,
           input: {
@@ -175,25 +218,28 @@ startHandler = function(handler, port, opt_host) {
       handler
     ]);
 
-    chain(request, function(responseMessage) {
-      var headers = responseMessage.headers || {};
-      headers['Content-Type'] = headers['Content-Type'] || 'text/plain';
-      if (responseMessage.output && responseMessage.output.body) {
-        headers['Content-Length'] = isBodyUtf8(responseMessage) ?
-            countUtf8Bytes(responseMessage.output.body) :
-            responseMessage.output.body.length;
-      }
-      response.sendHeader(responseMessage.status, headers);
-
-      if (responseMessage.output && responseMessage.output.body) {
-        if (isBodyUtf8(responseMessage)) {
-          response.write(responseMessage.output.body, 'utf8');
-        } else {
-          response.write(responseMessage.output.body);
+    handlerChain(request, function(responseMessage) {
+      // if responseMessage is missing, response has been streamed out already
+      if (responseMessage) {
+        var headers = responseMessage.headers || {};
+        headers['Content-Type'] = headers['Content-Type'] || 'text/plain';
+        if (responseMessage.output && responseMessage.output.body) {
+          headers['Content-Length'] = isBodyUtf8(responseMessage) ?
+              countUtf8Bytes(responseMessage.output.body) :
+              responseMessage.output.body.length;
+        }
+        response.writeHead(responseMessage.status, headers);
+  
+        if (responseMessage.output && responseMessage.output.body) {
+          if (isBodyUtf8(responseMessage)) {
+            response.write(responseMessage.output.body, 'utf8');
+          } else {
+            response.write(responseMessage.output.body);
+          }
         }
       }
 
-      response.close();
+      response.end();
 
       var end = Date.now();
       log('elapsed=' + (end - start) + 'ms');
@@ -209,7 +255,7 @@ startHandler = function(handler, port, opt_host) {
  */
 startPathHandlers = function(pathPrefixHandlers, port, opt_host) {
   var pathHandlers = handlePathPrefixArray.create(pathPrefixHandlers);
-  var rootHandler = CSF_TRACE ? createHandlerChain([
+  var rootHandler = CSF_TRACE ? chain.create([
       createAroundFilter(dumpRequest, dumpResponse), pathHandlers]) :
       pathHandlers;
   startHandler(rootHandler, port, opt_host);
@@ -220,7 +266,8 @@ startPathHandlers = function(pathPrefixHandlers, port, opt_host) {
 
 exports.handler = {
   handlePathPrefixArray: handlePathPrefixArray,
-  chain: createHandlerChain
+  chain: chain,
+  proxy: proxy
 };
 
 exports.filter = {
